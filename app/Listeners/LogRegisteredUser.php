07c564a8b13ca815fa5f6c95e7e562d254dbb31e
Vytautas Narkevicius <vytautasns@gmail.com>
2017-07-10 12:07:40 +0300

    Added user settings

---
+++ app/Listeners/LogRegisteredUser.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Setting;
+
+class LogRegisteredUser
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  Registered  $event
+     * @return void
+     */
+    public function handle(Registered $event)
+    {
+
+      $user = $event->user;
+
+      Setting::create([
+       'user_id' => $user->id,
+       'name' => 'first_login',
+       'value' => '1',
+      ]);
+
+      Setting::create([
+        'user_id' => $user->id,
+        'name' => 'weight_units',
+        'value' => 'kg',
+      ]);
+
+    }
+}
