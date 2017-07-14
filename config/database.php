538a4b688791daed5584ae95df6971202ffb0862
Vytautas Narkevicius <vytautasns@gmail.com>
2017-07-14 16:31:04 +0300

    mysql added

--- config/database.php
+++ config/database.php
@@ -1,5 +1,19 @@
 <?php
 
+// Database configuration
+if ($url = env('CLEARDB_DATABASE_URL', false)) {
+    $parts = parse_url($url);
+    $host = $parts["host"];
+    $username = $parts["user"];
+    $password = $parts["pass"];
+    $database = substr($parts["path"], 1);
+} else {
+    $host = env('DB_HOST', 'localhost');
+    $username = env('DB_USERNAME', 'forge');
+    $password = env('DB_PASSWORD', '');
+    $database = env('DB_DATABASE', 'forge');
+}
+
 return [
 
     /*
@@ -13,7 +27,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -40,19 +54,17 @@ return [
         ],
 
         'mysql' => [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-        ],
+         'driver' => 'mysql',
+         'host' => $host,
+         'database' => $database,
+         'username' => $username,
+         'password' => $password,
+         'charset' => 'utf8',
+         'collation' => 'utf8_unicode_ci',
+         'prefix' => '',
+         'strict' => true,
+         'engine' => null,
+     ],
 
         'pgsql' => [
             'driver' => 'pgsql',
